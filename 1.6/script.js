0d31a3484e73a2d4d852dfaf2d0b3f45ad4dce8f
Mahmudul Hasan Khosru <mahmudul@Mahmuduls-MacBook-Pro.local>
2023-02-22 12:11:29 +0600

    Vanilla Redux and Vanilla JS completed Redux workflow

--- 1.6/script.js
+++ 1.6/script.js
@@ -1,15 +1,49 @@
+// catach all DOM element
 const displayText = document.getElementById('displaytext');
-const inrementButton = document.getElementById('incrementbtn');
+const incrementButton = document.getElementById('incrementbtn');
 const decrementButton = document.getElementById('decrementbtn')
 
-let count = 0;
-inrementButton.addEventListener('click', ()=>{
+// initial State declear 
+const initialState = {
+    value:0,
+ };
 
-    count ++;
-    displayText.innerText = count;
+//  create a reducer function 
+const counterReducer = (state = initialState, action) =>{
+    if (action.type === 'increment'){
+        return{
+            ...state,
+            value: state.value + 1,
+        }
+    } else if (action.type === 'decrement'){
+        return{
+            ...state,
+            value: state.value - 1
+        }
+    } else return state;
+}
+
+// create  Redux Store 
+const store = Redux.createStore(counterReducer);
+
+const renderUI = () =>{
+    const updatedState = store.getState(); 
+    // getState is responsible for getting the state 
+    displayText.innerText = updatedState.value.toString();
+}
+// If state change then subscriber will be called
+store.subscribe(renderUI);
+
+// update UI initially
+renderUI();
+incrementButton.addEventListener('click', ()=>{
+    store.dispatch({
+        type: 'increment'
+    })
 })
 
 decrementButton.addEventListener('click', ()=>{
-    count --;
-    displayText.innerText = count;
+    store.dispatch({
+        type: 'decrement'
+    })
 })
